9eb37ee7732a1b00565a09a4023fb697490e2b66
Nicholas Bowman <nbowman@stanford.edu>
2016-11-13 00:08:17 -0800

    gives output data in id, score, date format

--- scripts/textSentiment.py
+++ scripts/textSentiment.py
@@ -1,5 +1,5 @@
 ########### Python 2.7 #############
-import httplib, urllib, base64, sys
+import httplib, urllib, base64, sys, json
 from test import scrapeArticles
 
 if (len(sys.argv) != 4):
@@ -18,37 +18,50 @@ params = urllib.urlencode({
 
 idCount = 1
 
-try:
-    body_list = [{"language":"en", "id":str(idCount), "text": "this is some text to analyze the sentiment of"}]
-    idCount+=1
-    """body = str({"documents": [
-        {
-            "language": "en",
-            "id":  "1",
-            "text": "this is some text to analyze the sentiment of"
-        }
-    ]})"""
-    for text, date in articleDictionary.iteritems():
-        if sys.getsizeof(text) < 10240:
-            temp = {}
-            temp["language"] = "en"
-            temp["id"] = str(idCount)
-            temp["text"] = text.encode('ascii', 'ignore')
-            print sys.getsizeof(temp["text"])
-            #temp["text"] = temp["text"][:len(temp["text"]) // 2]
-            print sys.getsizeof(temp["text"])
-            idCount+=1
-            body_list.append(temp)
-    
-    body = {}
-    body["documents"] = body_list
-    body = str(body)
+#try:
+body_list = []
+dateIdDict = {}
+"""body = str({"documents": [
+    {
+        "language": "en",
+        "id":  "1",
+        "text": "this is some text to analyze the sentiment of"
+    }
+]})"""
+for text, date in articleDictionary.iteritems():
+    encoded = text.encode('ascii', 'ignore')
+    if len(encoded) < 5120:
+        temp = {}
+        temp["language"] = "en"
+        temp["id"] = str(idCount)
+        encoded = text.encode('ascii', 'ignore')
+        temp["text"] = encoded
+        dateIdDict[idCount] = date
+        idCount+=1
+        body_list.append(temp)
 
-    conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
-    conn.request("POST", "/text/analytics/v2.0/sentiment?%s" % params, body, headers)
-    response = conn.getresponse()
-    data = response.read()
-    print(data)
-    conn.close()
-except Exception as e:
-    print("[Errno {0}] {1}".format(e.errno, e.strerror))
+    
+
+body = {}
+body["documents"] = body_list
+body = str(body)
+
+conn = httplib.HTTPSConnection('westus.api.cognitive.microsoft.com')
+conn.request("POST", "/text/analytics/v2.0/sentiment?%s" % params, body, headers)
+response = conn.getresponse()
+data = response.read()
+print(data)
+data = json.loads(data)
+conn.close()
+results_list = data["documents"]
+finalDict = {}
+for result in results_list:
+    new_list = []
+    new_list.append(str(result["score"]))
+    new_list.append(dateIdDict[int(result["id"])])
+    finalDict[result["id"]] = new_list
+print finalDict
+#return finalDict
+
+#except Exception as e:
+ #   print("[Errno {0}] {1}".format(e.errno, e.strerror))
